28df1ffd126b7df08193241e2ad8062a08d6bfe6
Giovani Murakami <giovani.murakami@outlook.com>
2023-04-18 22:25:09 -0300

    update - Adicionado validação de senha e user

--- Js_Ts/exercícios/validarFormulário/assets/js/main.js
+++ Js_Ts/exercícios/validarFormulário/assets/js/main.js
@@ -11,7 +11,34 @@ class ValidaFormulario {
     handleSubmit(event) {
         event.preventDefault()
         const camposValidos = this.camposSaoValidos()
+        const senhasValidas = this.senhasSaoValidas()
+
+        if (camposValidos && senhasValidas) {
+            alert('Formulário enviado')
+            this.formulario.submit()
+        }
     }
+
+    senhasSaoValidas() {
+        let valid = true
+
+        const senha = this.formulario.querySelector('.senha')
+        const senha2 = this.formulario.querySelector('.senha2')
+
+        if (senha.value !== senha2.value){
+            valid = false
+            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais')
+            this.criaErro(senha2, 'Campos senha e repetir senha precisam ser iguais')
+        }
+
+        if (senha.value.length < 6 || senha.value.length > 12) {
+            valid = false
+            this.criaErro(senha, 'Senha precisa ter de 3 a 12 chars!')
+        }
+
+        return valid
+    }
+
     camposSaoValidos() {
         let valid = true
         
@@ -29,7 +56,13 @@ class ValidaFormulario {
             if(campo.classList.contains('cpf')) {
                 if(!this.validaCPF(campo)) valid = false
             }
+
+            if(campo.classList.contains('usuario')) {
+                if(!this.validaUser(campo)) valid = false
+            }
         }
+
+        return valid
     }
 
     validaCPF(campo) {
@@ -37,6 +70,19 @@ class ValidaFormulario {
         if (!cpf.validate()) {
             this.criaErro(campo, 'CPF Inválido')
             return false
+        }
+        return true
+    }
+
+    validaUser(campo){
+        const user = campo.value
+        if (user.length < 3 || user.length > 12 ) {
+            this.criaErro(campo, 'Usuário inválido, é necessário ter entre 3 e 12 chars!')
+            valid = false
+        }
+        if (!user.match(/[a-zA-Z0-9]/g)){
+            this.criaErro(campo, 'Nome de usuário precisa conter apenas letras e/ou números!')
+            valid = false
         }
         return true
     }
